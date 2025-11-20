5fc2535cd1a909a3f045b6bb35790a403e98cc67
annavanong <anna.vanongeval@student.howest.be>
2025-11-20 13:07:22 +0100

    feat: add GitHub Actions workflow for deployment and update site configuration

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const postsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
+  schema: ({ }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+    }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
+
